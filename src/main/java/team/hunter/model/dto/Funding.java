d3d06b8530da25b645dfef14d6bdaaef52feb25f
chanho <cksghfkddl@naver.com>
2020-06-17 19:36:35 +0900

    commit

---
+++ src/main/java/team/hunter/model/dto/Funding.java
@@ -0,0 +1,35 @@
+package team.hunter.model.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Funding {
+	private int code;
+	private int mdCode;
+	private String title;
+	private String rewardName;
+	private int rewardPrice;
+	private int goalPrice;
+	private int stackPrice;
+	private String image;
+	private String category;
+	private String openDate;
+	private String endDate;
+	private String fundingState;
+	
+	//좋아요 갯수 때문에 추가
+	private int cnt;
+	
+	private Member member;
+	
+
+	private List<Member> memberList;
+}
